536b6850ee5586fac40e90913a9d5949f09d4d28
BonMarty <bonmarty0207@gmail.com>
2025-12-16 09:54:15 +0300

    feat: add prisma client

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,5 +1,4 @@
-import { Controller, Get, Req } from '@nestjs/common';
-import type { FastifyRequest } from 'fastify';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,8 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Req() req: FastifyRequest): string {
-    console.log(req);
+  getHello(): string {
     return this.appService.getHello();
   }
 }
