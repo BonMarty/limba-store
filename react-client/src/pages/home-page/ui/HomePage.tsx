d50458bc4530523b1627e5ac79b33fe0eb4eabca
BonMarty <BonMarty0207@gmail.com>
2025-12-18 03:53:43 +0300

    fix: change link to products page from inline string to routes

--- react-client/src/pages/home-page/ui/HomePage.tsx
+++ react-client/src/pages/home-page/ui/HomePage.tsx
@@ -1,3 +1,4 @@
+import { routes } from "@/shared/routes";
 import { Button } from "@/shared/ui";
 import { Link } from "react-router";
 
@@ -5,7 +6,7 @@ export function HomePage() {
   return (
     <div className="space-y-6">
       <h1 className='text-4xl'>LImba Store.</h1>
-      <Link to={"/products"}>Products</Link>
+      <Link to={routes.PRODUCTS.to}>Products</Link>
       <div className="flex items-center gap-2">
         <Button className="px-4 py-1.5 rounded transition-colors duration-300 cursor-pointer text-white bg-sky-500 hover:bg-blue-500">Click me!</Button>
         <Button className="px-4 py-1.5 rounded transition-colors duration-300 cursor-pointer text-white bg-fuchsia-500 hover:bg-purple-500">Click me!</Button>
