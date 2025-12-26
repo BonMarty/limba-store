298db0081238dfb9bd1b1dc0432510b7ff43ef07
BonMarty <bonmarty0207@gmail.com>
2025-12-26 09:27:35 +0300

    fix: remove password from get user request and add authorization decorator for create order request

--- nest-server/src/modules/order/order.controller.ts
+++ nest-server/src/modules/order/order.controller.ts
@@ -1,11 +1,5 @@
-import {
-  Body,
-  ClassSerializerInterceptor,
-  Controller,
-  Get,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Authorization } from 'src/common/decorators';
 import { CreateOrderDto } from './dto';
 import { OrderService } from './order.service';
 
@@ -13,12 +7,12 @@ import { OrderService } from './order.service';
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
-  @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   findAll() {
     return this.orderService.findAll();
   }
 
+  @Authorization()
   @Post('create')
   create(@Body() dto: CreateOrderDto) {
     return this.orderService.create(dto);
--- nest-server/src/modules/user/user.service.ts
+++ nest-server/src/modules/user/user.service.ts
@@ -10,6 +10,9 @@ export class UserService {
       where: {
         id,
       },
+      omit: {
+        password: true,
+      },
     });
 
     return user;
