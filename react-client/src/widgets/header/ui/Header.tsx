015d8b91072e656ff1413fa7fbe9c93dfcb9b937
BonMarty <bonmarty0207@gmail.com>
2025-12-27 02:57:12 +0300

    fix: add link to auth page for dev

--- react-client/src/widgets/header/ui/Header.tsx
+++ react-client/src/widgets/header/ui/Header.tsx
@@ -5,9 +5,7 @@ export function Header() {
   return (
     <header className="flex justify-between items-center gap-4 p-4">
       <Link to={routes.HOME.to} className="text-2xl font-semibold">LImba Store.</Link>
-      <nav className="flex items-center gap-2">
-        <Link to={routes.PRODUCTS.to}>Products</Link>
-      </nav>
+      <Link to={routes.AUTH.to}>Auth</Link>
     </header>
   )
 }
