0eb0b7187267ea6b817eec0de67667351b787fe4
BonMarty <bonmarty0207@gmail.com>
2026-01-06 13:36:45 +0300

    fix: add missing file with changes

--- react-client/src/features/cart/ui/Cart.tsx
+++ react-client/src/features/cart/ui/Cart.tsx
@@ -1,11 +1,31 @@
-import { useGetMyCartQuery } from "../api"
+import { Button } from "@/shared/ui"
+import { useClearCartMutation, useGetMyCartQuery } from "../api"
 
 export function Cart() {
-  const { data } = useGetMyCartQuery("")
+  const { isLoading, error, data } = useGetMyCartQuery("")
+  const [trigger] = useClearCartMutation()
 
-  console.log("data", data)
+  if (isLoading) return <div>Loading...</div>
+
+  if (error) return <div>{JSON.stringify(error)}</div>
+
+  if (!data) return <div>No data</div>
 
   return (
-    <div>Cart</div>
+    <div className="space-y-4">
+      <div className="flex items-center gap-4">
+        <h2 className="font-semibold text-2xl">Cart</h2>
+        <Button onClick={() => trigger()}>Clear cart</Button>
+      </div>
+      <p><strong>Total price:</strong> {data.totalPrice}</p>
+      <ul className="space-y-4">
+        {data.items.map((item) => (
+          <li key={item.id} className="p-2 rounded bg-zinc-800">
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
